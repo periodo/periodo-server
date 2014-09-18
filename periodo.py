f4191cabae76b9b69792d0a52b386c85c4896401
Patrick Golden <ptgolden0@gmail.com>
2014-09-18 15:26:04 -0400

    Implement basic framework of patch CRUD

--- periodo.py
+++ periodo.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import datetime
 from email.utils import parsedate
 import json
@@ -5,8 +6,12 @@ import sqlite3
 from time import mktime
 from wsgiref.handlers import format_date_time
 
-from flask import Flask, abort, g
-from flask.ext.restful import Api, Resource, fields, marshal_with, reqparse
+from jsonpatch import JsonPatch, JsonPatchException
+from jsonpointer import JsonPointerException
+
+from flask import Flask, abort, g, request
+from flask.ext.restful import (Api, Resource, fields, marshal, marshal_with,
+                               reqparse)
 
 __all__ = ['init_db', 'app']
 
@@ -38,20 +43,46 @@ def add_cors_headers(response):
 
 ISO_TIME_FMT = '%Y-%m-%d %H:%M:%S'
 
+class InvalidPatchException(Exception):
+    pass
+
 def iso_to_timestamp(iso_timestr, fmt=ISO_TIME_FMT):
     dt = datetime.datetime.strptime(iso_timestr, fmt)
     return mktime(dt.timetuple())
 
-dataset_parser = reqparse.RequestParser()
-dataset_parser.add_argument('If-Modified-Since', dest='modified', location='headers')
+def patch_from_text(patch_text):
+    patch_text = patch_text or ''
+    if isinstance(patch_text, bytes):
+        patch_text = patch_text.decode()
+    try:
+        patch = json.loads(patch_text)
+    except:
+        raise InvalidPatchException('Patch data could not be parsed as JSON.')
+    patch = JsonPatch(patch)
+    return patch
 
+def validate_patch(patch, dataset=None):
+    dataset = dataset or query_db('select * from dataset order by created desc', one=True)
+    
+    # Test to make sure it will apply
+    try:
+        patch.apply(json.loads(dataset['data']))
+    except JsonPatchException:
+        raise InvalidPatchException('Not a valid JSON patch.')
+    except JsonPointerException:
+        raise InvalidPatchException('Could not apply JSON patch to dataset.')
+
+class JsonField(fields.Raw):
+    def format(self, value):
+        return json.loads(value)
 
 #################
 # API Resources #
 #################
 
 index_fields = {
-    'dataset': fields.Url('dataset', absolute=True)
+    'dataset': fields.Url('dataset', absolute=True),
+    'patches': fields.Url('patchlist', absolute=True)
 }
 class Index(Resource):
     @marshal_with(index_fields)
@@ -75,13 +106,133 @@ class Dataset(Resource):
         return json.loads(dataset['data']), 200, {
             'Last-Modified': format_date_time(last_modified),
         }
+    def patch(self):
+        try:
+            dataset = self._get_latest_dataset()
+            patch = patch_from_text(request.data)
+            validate_patch(patch, dataset)
+        except InvalidPatchException as e:
+            return { 'status': 400, 'message': str(e) }, 400
+
+        FIGURE_OUT_USERNAME = 'someone'
+        username = FIGURE_OUT_USERNAME
+
+        db = get_db()
+        curs = db.cursor()
+        curs.execute(
+            'insert into patch_request (created_by, created_from) values (?, ?);',
+            (username, dataset['id'])
+        )
+        patch_id = curs.lastrowid
+        curs.execute(
+            'insert into patch_text (created_by, patch_request, text) values(?, ?, ?);',
+            (username, patch_id, patch.to_string())
+        )
+        db.commit()
+
+        return None, 202, {
+            'Location': api.url_for(PatchRequest, id=patch_id)
+        }
+
+PATCH_QUERY = """
+select * 
+from patch_request
+left join (
+    select
+        id as current_text,
+        max(created_at) as updated_at,
+        created_by as updated_by,
+        text,
+        patch_request
+    from patch_text
+    group by patch_request
+) as subq
+on patch_request.id = subq.patch_request
+"""
+
+patch_list_fields = OrderedDict((
+    ('url', fields.Url('patchrequest', absolute=True)),
+    ('created_by', fields.String),
+    ('created_at', fields.String),
+    ('updated_by', fields.String),
+    ('updated_at', fields.String),
+    ('created_from', fields.String),
+    ('applied_to', fields.String),
+    ('text', fields.Url('patch', absolute=True)),
+    ('open', fields.Boolean),
+    ('merged', fields.Boolean)
+))
+
+def make_dataset_url(version):
+    return api.url_for(Dataset, _external=True) + '?version=' + str(version)
+
+def process_patch_row(row):
+    d = dict(row)
+    d['created_from'] = make_dataset_url(row['created_from'])
+    d['applied_to'] = make_dataset_url(row['created_from']) if row['applied_to'] else None
+    return d
+
+class PatchList(Resource):
+    def get(self):
+        query = PATCH_QUERY
+        rows = query_db(PATCH_QUERY)
+        data = [process_patch_row(row) for row in rows]
+        return marshal(data, patch_list_fields)
+
+patch_fields = patch_list_fields.copy()
+patch_fields.update((
+    ('mergeable', fields.Boolean),
+))
+class PatchRequest(Resource):
+    def get(self, id):
+        row = query_db(PATCH_QUERY + ' where id = ?', (id,), one=True)
+        if not row:
+            abort(404)
+
+        data = process_patch_row(row)
+        data['mergeable'] = True
+
+        dataset = query_db('select * from dataset order by created desc', one=True)
+        patch = patch_from_text(data['text'])
+        try:
+            patch.apply(json.loads(dataset['data']))
+        except JsonPatchException:
+            data['mergeable'] = False
+        except JsonPointerException:
+            data['mergeable'] = False
+
+        return marshal(data, patch_fields)
+
+class Patch(Resource):
+    def get(self, id):
+        row = query_db(PATCH_QUERY + ' where id = ?', (id,), one=True)
+        return json.loads(row['text']), 200
+    def put(self, id):
+        try:
+            patch = patch_from_text(request.data)
+            validate_patch(patch)
+        except InvalidPatchException as e:
+            if str(e) != 'Could not apply JSON patch to dataset.':
+                return { 'status': 400, 'message': str(e) }, 400
+
+        db = get_db()
+        curs = db.cursor()
+        curs.execute(
+            'insert into patch_text (created_by, patch_request, text) values(?, ?, ?);',
+            ('SOMEONE', id, patch.to_string())
+        )
+        db.commit()
+
 
 ###############
 # API Routing #
 ###############
 
-api.add_resource(Index, '/', endpoint='index')
-api.add_resource(Dataset, '/dataset/', endpoint='dataset')
+api.add_resource(Index, '/')
+api.add_resource(Dataset, '/dataset/')
+api.add_resource(PatchList, '/patches/')
+api.add_resource(PatchRequest, '/patches/<int:id>/')
+api.add_resource(Patch, '/patches/<int:id>/patch.jsonpatch')
 
 
 ######################
--- schema.sql
+++ schema.sql
@@ -3,4 +3,34 @@ CREATE TABLE dataset (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	data TEXT NOT NULL
-)
+);
+
+DROP TABLE IF EXISTS patch_request;
+CREATE TABLE patch_request (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	created_by TEXT NOT NULL,
+
+	open BOOLEAN NOT NULL DEFAULT 1,
+	merged BOOLEAN NOT NULL DEFAULT 0,
+	merged_at TIMESTAMP,
+	merged_by TEXT,
+
+	created_from INTEGER NOT NULL,
+	applied_to INTEGER,
+
+	FOREIGN KEY(created_from) REFERENCES dataset(id),
+	FOREIGN KEY(applied_to) REFERENCES dataset(id)
+);
+
+DROP TABLE IF EXISTS patch_text;
+CREATE TABLE patch_text (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_by TEXT NOT NULL,
+	patch_request INTEGER NOT NULL,
+	text TEXT NOT NULL,
+
+	FOREIGN KEY(patch_request) REFERENCES patch_request(id)
+);
+
