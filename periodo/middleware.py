26afdfe0c102788167c03ab769c7858f2d2b5f55
Ryan Shaw <ryanshaw@unc.edu>
2018-10-28 11:31:45 -0400

    ...

--- periodo/middleware.py
+++ periodo/middleware.py
@@ -3,5 +3,6 @@ class RemoveTransferEncodingHeaderMiddleware(object):
     def __init__(self, app):
         self.app = app
 
-    def __call__(self, env, start_response):
-        self.app.logger.error(env)
+    def __call__(self, environ, start_response):
+        print(environ)
+        return self.app(environ, start_response)
