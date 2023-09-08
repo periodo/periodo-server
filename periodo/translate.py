06be4e6d4f005855b8dc25429cd0af29f513ee7d
Ryan Shaw <ryanshaw@unc.edu>
2023-09-08 14:31:30 -0400

    use translation service rather than running a subprocess

--- periodo/translate.py
+++ periodo/translate.py
@@ -1,13 +1,12 @@
-import json
-import os
-import subprocess
-from pathlib import Path
+import httpx
+import random
+import time
+from uuid import uuid4
 from periodo import app
-from tempfile import NamedTemporaryFile, gettempdir
-from time import time
 
 
-TMPDIR = gettempdir()
+MAXIMUM_BACKOFF = 32.0
+MAXIMUM_POLLS = 10
 
 
 class RDFTranslationError(Exception):
@@ -23,120 +22,49 @@ class RDFTranslationError(Exception):
         )
 
 
-def read_file(filename):
-    with open(filename, encoding="utf8") as f:
-        return f.read()
-
-
-def run_subprocess(command_line, out_suffix):
-    sentinel = Path(TMPDIR) / "running-jvm"
-    try:
-        sentinel.touch(exist_ok=False)
-        start_time = time()
-        try:
-            app.logger.debug("Running subprocess:\n%s" % " ".join(command_line))
-            with NamedTemporaryFile(suffix=out_suffix, delete=False) as out:
-                with NamedTemporaryFile(suffix=".err", delete=False) as err:
-                    subprocess.run(
-                        command_line,
-                        stdout=out,
-                        stderr=err,
-                        encoding="utf8",
-                        env={"JVM_ARGS": "-Xms768M -Xmx768M"},
-                    )
-                    app.logger.debug("stdout: %s" % out.name)
-                    app.logger.debug("stderr: %s" % err.name)
-                    return (out.name, err.name)
-        finally:
-            app.logger.debug(f"elapsed time: {time() - start_time}s")
-            try:
-                sentinel.unlink()
-            except FileNotFoundError:
+def wait_for_translation(client: httpx.Client, url: str) -> str:
+    for n in range(0, MAXIMUM_POLLS):
+        response = client.get(url)
+        match response.status_code:
+            case httpx.codes.ACCEPTED:
                 pass
-    except FileExistsError:
-        app.logger.debug("jvm is already running; returning 503")
-        raise RDFTranslationError(code=503) from None
-
-
-def triples_to_csv(triples_file):
-    return run_subprocess(
-        [
-            app.config["ARQ"],
-            "--data",
-            triples_file,
-            "--query",
-            app.config["CSV_QUERY"],
-            "--results=CSV",
-        ],
-        ".csv",
-    )
-
-
-def jsonld_to(serialization, jsonld):
-    with NamedTemporaryFile(suffix=".jsonld") as f:
-        f.write(json.dumps(jsonld, ensure_ascii=False).encode())
-        f.flush()
-        return run_subprocess(
-            [
-                app.config["RIOT"],
-                "--syntax=jsonld",
-                "--formatted=%s" % serialization,
-                f.name,
-            ],
-            "." + serialization,
-        )
-
-
-def looks_like(serialization, data):
-    # checking the return code is not reliable, because riot/arq will return 1
-    # even if there are only warnings. So we look at the first character of
-    # output as a hint
-    if len(data) == 0:
-        return False
-    if serialization == "ttl":
-        return data[0] == "@"  # @prefix
-    if serialization == "csv":
-        return data[0] == "p"  # period
-    if serialization == "nt":
-        return data[0] == "<" or data[0] == "_"  # URI or blank node
-    return False
-
-
-def log_error(stdout, stderr):
-    app.logger.error("stdout:\n%s" % stdout)
-    app.logger.error("stderr:\n%s" % stderr)
-
-
-def jsonld_to_turtle(jsonld):
-    turtle_file, errors_file = jsonld_to("ttl", jsonld)
-    try:
-        turtle = read_file(turtle_file)
-        if not looks_like("ttl", turtle):
-            log_error(turtle, read_file(errors_file))
-            raise RDFTranslationError()
-        return turtle
-    finally:
-        os.remove(turtle_file)
-        os.remove(errors_file)
-
-
-def jsonld_to_csv(jsonld):
-    triples_file, errors_file_1 = jsonld_to("nt", jsonld)
-    try:
-        triples = read_file(triples_file)
-        if not looks_like("nt", triples):
-            log_error(triples, read_file(errors_file_1))
-            raise RDFTranslationError()
-        csv_file, errors_file_2 = triples_to_csv(triples_file)
-        try:
-            csv = read_file(csv_file)
-            if not looks_like("csv", csv):
-                log_error(csv, read_file(errors_file_2))
+            case httpx.codes.OK:
+                return response.text
+            case _:
+                app.logger.error(
+                    f"Translation failed with {response.status_code}: {response.text}"
+                )
                 raise RDFTranslationError()
-            return csv
-        finally:
-            os.remove(csv_file)
-            os.remove(errors_file_2)
-    finally:
-        os.remove(triples_file)
-        os.remove(errors_file_1)
+        wait_time = min(((2 ^ n) + (random.randrange(1000) / 1000.0)), MAXIMUM_BACKOFF)
+        time.sleep(wait_time)
+    app.logger.error("Translation timed out")
+    raise RDFTranslationError()
+
+
+def jsonld_to(serialization: str, jsonld: dict | list) -> str:
+    uuid = uuid4()
+    path = f"{uuid}.{serialization}"
+    url = f"http://periodo-translator.internal:8080/{path}"
+
+    with httpx.Client() as client:
+        response = client.put(
+            url,
+            json=jsonld,
+            headers={"Content-Type": "application/ld+json; charset=UTF-8"},
+        )
+        match response.status_code:
+            case httpx.codes.ACCEPTED:
+                return wait_for_translation(client, url)
+            case _:
+                app.logger.error(
+                    f"Translation failed with {response.status_code}: {response.text}"
+                )
+                raise RDFTranslationError()
+
+
+def jsonld_to_turtle(jsonld: dict | list) -> str:
+    return jsonld_to("ttl", jsonld)
+
+
+def jsonld_to_csv(jsonld: dict | list) -> str:
+    return jsonld_to("csv", jsonld)
